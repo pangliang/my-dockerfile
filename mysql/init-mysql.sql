abd4965952f089b35548c8a839308772324a3784
pangliang <418094911@qq.com>
2017-05-07 00:01:35 +0800

    删掉mysql和php5, mysql官方的就很好用

--- mysql/init-mysql.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM mysql.user ;
-CREATE USER 'root'@'%' IDENTIFIED BY 'pangliang!@#$%^' ;
-GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
-DROP DATABASE IF EXISTS test ;
-FLUSH PRIVILEGES ;
