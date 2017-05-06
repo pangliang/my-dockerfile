ce712f0e5334a1de5536bf81249defb005b78678
pangliang <418094911@qq.com>
2017-05-06 23:04:55 +0800

    update

--- php7-ssh-server/run.sh
+++ php7-ssh-server/run.sh
@@ -9,5 +9,13 @@ HTTP_PORT=21080
 
 docker stop $SERVER_NAME
 docker rm $SERVER_NAME
-docker run -d -p $HTTP_PORT:80 -p $SSH_PORT:22 -v $HOME:/data -v $HOME/sites/default.conf:/server/tengine/conf/sites/default.conf --name=$SERVER_NAME pangliang/php7-ssh-server
+docker run -d -p $HTTP_PORT:80 -p $SSH_PORT:22 \
+	-v $HOME:/data \
+	-v $HOME/authorized_keys:/root/.ssh/ \
+	-v $HOME/nginx.conf:/server/tengine/conf/ \
+	-v $HOME/sites:/server/tengine/conf/sites \
+	-v $HOME/www.conf:/etc/php/7.0/fpm/pool.d/ \
+	-v $HOME/php-fpm.conf:/etc/php/7.0/fpm/ \
+	-v $HOME/sshd_config:/etc/ssh/ \
+	--name=$SERVER_NAME pangliang/php7-ssh-server
 
